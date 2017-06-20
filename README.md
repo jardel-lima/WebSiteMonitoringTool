89994601fff8088235813a8ceca1f9152df96ad1
jardel-lima <jardelribeiro.lima@gmail.com>
2017-06-20 17:23:03 -0300

    Frist Commit

---
+++ README.md
@@ -0,0 +1,100 @@
+##WEBSITE MONITORING TOOL##
+
+
+#DESCRIPTION
+
+This script will monitor Service Level Indicators (SLI) of a list of websites. Two SLIs will be monitored:
+SUCCESSFUL RESPONSE and FAST RESPONSE.
+    - SUCCESSFUL RESPONSE - Responses with HTTP status code >= 200 and <= 499;
+    - FAST RESPONSE - HTTP requests that are responded in 100ms or less.
+
+For a list of website url's a get request will be made every 5 seconds. Each SLI will compared with the given
+Service Level Objective (SLO).
+
+
+#REQUIREMENTS
+
+    - Python 2.7
+
+
+#INSTALLATION
+
+    - Clone project or copy the  websitemonitor.py and website_monitoring_tool.py files to the same folder
+
+
+#EXECUTION
+
+    - First create a text file with website urls and their respective Service Level Objectives with the
+    following format:
+    <url>;<successful response SLO>;<fast response SLO>
+        - url must be a string with a http format
+        - the other 2 must be doubles >= 0.0 and <=100.0
+
+    - Open the terminal and execute:
+        python website_monitoring_tool.py <text file with website urls and SLOs>
+
+    - If every thing went well you will see the message: 'Monitoring has started!'
+
+
+#USAGE
+
+When the script was executed you will see a simple option menu:
+    Menu:
+    s - Show Status
+    r - Save Report
+    q - Exit
+    Choice:
+
+    When you type and enter one of those options, the following will happen:
+
+    s - Will be displayed on the terminal the current SLI the SLO and the STATUS
+    (if SLI >= SLO GOOD will be displayed otherwise BAD);
+
+    r - The same output seen on terminal when s is selected will be save on a text file called report.txt;
+
+    q - The aplication will stop.
+
+
+#DEVELOPMENT CONSIDERATIONS
+
+This application was writen using Python for 3 reasons:
+    1 - It is very simple and powerful programming language;
+    2 - It is the programming language that lately I have used the most.
+    3 - I had few time to concept and build the application.
+
+Once I was asked to build a long-running application with user interaction and with some network tasks
+I decided to use multithreading. It allows the user to get partial reports while the application keep making
+requests and make HTTP requests over Internet without 'blocking' the application.
+
+All threads was created as daemons, it allows the application to exit even if some threads still alive. The
+necessary modules on this applications are only default modules that comes with Python 2.7, it is not
+necessary to install any extra modules.
+
+I decided to create a class called WebSiteMonitor because it helps to get reports to each monitored website.
+I used a list of thread to each website because as every 5 second a new request has to be made
+and even though the last request made cannot be a fast response anymore, once it has passed 5 seconds, it can be
+a successful response still. So, I create a new thread to the new request and keep the old one in the list.
+Not to keep finished threads in the list, I remove threads that are not alive anymore.
+
+#IMPROVEMENTS AND ISSUES
+
+The first improvement on this application was to create a better user interface, although it does what
+it was asked for, this application is not user-friendly. A GUI to better user interaction and visualization
+can be done. Another alternative would be use this script as content provider to a web application with a better
+user interface.
+
+Another improvement would be finding a better way to calculate the request response time. Doing some fast
+research I found some modules that return the exactly response time to a request, I did not use this because
+I did not have enough time and I wanted to keep this script simple. During tests I found out that the some
+DNS servers can cause a 'Name or service not known' error when monitoring more than 15-20 websites.
+Some forums said that this is error can be caused be the DNS server. I did not have time to better investigate
+this error and find solutions, so I used a try catch as a workaround.
+
+#CREDITS
+
+This script was developed by Jardel Ribeiro de Lima (jardelribeiro.lima@gmail.com)
+
+
+
+
+
---
+++ website_monitoring_tool.py
@@ -0,0 +1,174 @@
+#Author: Jardel Ribeiro de Lima
+#Date: 07/20/2017
+#Local: Juazeiro-BA, Brazil
+
+import sys
+import threading
+import time
+import re
+
+from websitemonitor import WebSiteMonitor
+
+sites = []
+begin_time = None
+
+#Verify if Exist any thread alive
+def has_alive_thread(threads):
+    for thread in threads:
+        if thread.is_alive():
+            return True
+    return False
+
+#Validate inputs with regular expressions
+def validate_input(input):
+
+    match = re.search(r'^\d{1,2}(\.\d+){0,1}$',input[1].replace(',','.'))
+    if not match or not (float(input[1].replace(',','.')) >= 0 and float(input[1].replace(',','.')) <= 100 ):
+        return False
+
+    match = re.search(r'^\d{1,2}(\.\d+){0,1}$', input[2].replace(',','.'))
+    if not match or not (float(input[2].replace(',','.')) >= 0 and float(input[2].replace(',','.')) <= 100 ):
+        return False
+
+
+    match = re.search(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
+                      input[0])
+    if not match:
+        return False
+
+    return True
+
+#Read input file with URL's and SLO's
+def read_file(file):
+
+    sites=[]
+    try:
+        with open(file,'r') as file:
+            for line in file:
+                info = line.split(';')
+
+                if len(info) < 3:
+                    print('\nError on line - {}.\n'
+                          'Correct format is: <url>;<successful response SLO>;<fast response SLO>'.format(line.strip()))
+                else:
+                    if not validate_input(info):
+                        print('\nError with url format or double values on line - {}.\n'
+                          'Correct format is: <url>;<double>;<double>'.format(line.strip()))
+                        continue
+
+                    url = info[0].strip()
+
+                    if url[-1] == '/':
+                        url = url[:-1]
+
+                    site = WebSiteMonitor(url,float(info[1]),float(info[2]))
+                    sites.append(site)
+    except IOError:
+        print('Could not open file {}.'.format(file))
+        exit()
+
+    return sites
+
+#Thread responsable for making requests every 5 seconds
+def make_requests_thread():
+    while True:
+        for site in sites:
+            site.make_request()
+        time.sleep(5)
+
+#Mount string with websites status info
+def get_status_info():
+
+    info = 'SHOW WEBSITE STATUS\n'
+    info += 'BEGIN TIME {}\n'.format(time.strftime('%d/%m/%Y -- %H:%M:%S', begin_time))
+    info += 'NOW {}\n'.format(time.strftime('%d/%m/%Y -- %H:%M:%S'))
+    info += '{0:40s}\t{1:30s}\t{2:30s}\n'.format('', 'SUCCESSFUL RESPONSE', 'FAST RESPONSE')
+    info += '{0:40s}\t{1:10s}\t{2:10s}\t{3:10s}\t{4:10s}\t{5:10s}\t{6:10s}\t{7}\n' \
+        .format('URL', 'SLI', 'SLO', 'STATUS', 'SLI', 'SLO', 'STATUS','OBS')
+
+    for site in sites:
+
+        if site.req_n == 0:
+            info += '{0:30s}\t{1}\n'.format(site.url, 'NO REQUESTS')
+            continue
+
+        sli_succ_resp = float(site.succ_res_n / float(site.req_n)) * 100
+        sli_fast_resp = float(site.fast_res_n / float(site.req_n)) * 100
+
+        if sli_fast_resp < site.fast_res_slo:
+            fast_sli_status = 'BAD'
+        else:
+            fast_sli_status = 'GOOD'
+
+        if sli_succ_resp < site.succ_res_slo:
+            succ_sli_status = 'BAD'
+        else:
+            succ_sli_status = 'GOOD'
+
+        if has_alive_thread(site.threads):
+            alive_threads = '*'
+        else:
+            alive_threads = ''
+
+        if site.dns_error:
+            dns_error = '+'
+        else:
+            dns_error = ''
+
+        info += '{0:40s}\t{1:10.2f}%\t{2:10.2f}%\t{3:10s}\t{4:10.2f}%\t{5:10.2f}%\t{6:10s}\t{7}{8}\n'.format(
+            site.url,
+            sli_succ_resp,
+            site.succ_res_slo,
+            succ_sli_status,
+            sli_fast_resp,
+            site.fast_res_slo,
+            fast_sli_status,
+            alive_threads,
+            dns_error)
+    info += 'Legend:\n* - Waiting for succesful responses\n+ - DNS Error'
+
+    return info
+
+#Save report with websites' status
+def save_report():
+    print('Save status to report.txt')
+    try:
+        with open('report.txt','w') as file:
+            file.write(get_status_info())
+    except IOError:
+        print('Could not save file report.txt!')
+
+#Display website's status on terminal
+def show_status():
+    print(get_status_info())
+
+
+#Main
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        print('usage: python website_monitoring_tool.py <file_with_sites_urls_and_slos>')
+
+    else:
+        print('Web Site SLO Monitor')
+        print('\nReading file with URL\'s and SLO\'s')
+
+        sites = read_file(sys.argv[1])
+        thread = threading.Thread(target=make_requests_thread)
+        thread.daemon = True
+        thread.start()
+        begin_time = time.localtime()
+
+        print('\nMonitoring has started!')
+
+        while True:
+            print('\nMonitoring...')
+            print('Menu:\ns - Show Status\nr - Save Report\nq - Exit')
+            input = raw_input('Choice:')
+
+            if input == 'q':
+                exit(0)
+            elif input == 's':
+                show_status()
+            elif input == 'r':
+                save_report()
